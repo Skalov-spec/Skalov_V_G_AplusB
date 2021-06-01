b95f0c26421398c3f92b22c39eec66d110aa99b3
Skalov-spec <85168442+Skalov-spec@users.noreply.github.com>
2021-06-01 15:13:53 +0300

    Add files via upload

---
+++ Skalov_V_G_A+B/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Numerics;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger a, b;
+            a = BigInteger.Parse(Console.ReadLine());
+            b = BigInteger.Parse(Console.ReadLine());
+            Console.WriteLine(a+b);       
+        }
+    }
+}
